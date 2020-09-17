baa37ffcf9579658769606fcc192d80b3346a330
gummangummangumman <trygve@igesund.com>
2020-09-17 17:38:59 +0200

    Move some data to own js file and only replace emotes with spaces around them

--- javascript/twitch.js
+++ javascript/twitch.js
@@ -1,50 +1,7 @@
 var spamming = false;
 var darkMode = false;
 var spamType = "laughing";
-
-var usernamePrefixes = ["crazy", "universal", "sick", "insane", "cool", "fun", "mad", "generic", "Cpt", "nice", "adolf", "Dan", "VAC", "SWE", "Wizard", "faceless", "olof",
-                        "best_", "daddy", "mama", "mister_", "davai", "Nick", "de_", "the_", "iAm", "Loungin", "extra", "BOT", "dirty", "shoutout_to_", "devil", "Only"];
-
-var usernameSuffixes = ["Kappa", "Sniper", "marshmellow", "Shrek", "M", "LUL", "Games", "HD", "lolo", "_yolo", "QQ", "stone", "Trump", "xD", "meister", "eric", "james", "loser", "haha", "noob", "dude", "Bro",
-                       "shotgun", "DADDY", "OneTaps", "winner", "xx", "pepe", "explosion", "easy", "Nut", "000", "Biceps", "gamer", "Majestic", "zzzzz", "vortex", "sound", "tv"];
-
-var usernameColors = ["red", "green", "#40b7b5", "blue", "purple", "#aa9929"];
-
-var laughingMessages = ["OMEGALUL", "OMEGALUL Clap", "LULW", "KEKW"];
-var positiveMessages = ["VAC", "VAC", "VAC", "VAC", "VAC", "VAC", "WTF", "LUL", "holy shit","SAVED",
-"ez", "GG", "Kappa", "4Head", "WHAT", "PogChamp", "PogChamp PogChamp PogChamp PogChamp PogChamp",
-"KQLY STYLE", "ANY NAFFERS SeriousSloth", "Kreygasm"];
-var negativeMessages = ["BOT", "BOT", "BOT", "BOT", "BOT", "NA CS", "NA CS", "WTF",
-"LUL", "HAHAHAHA", "OMG", "LMAO", "so bad omg", "xD", "gg", "RUINED", "Kappa", "WutFace",
-"NotLikeThis", "4Head", "OMEGALUL", "OMEGALUL Clap", "JAME TIME", "BibleThump"];
-var bobRossMessage = ["SAVED", "RUINED", "SAVED", "RUINED", "CoolStoryBob"];
-
-
-var emotes = [
-    ["Kappa", "kappa.png"],
-    ["WutFace", "wutface.png"],
-    ["4Head", "4head.png"],
-    ["CoolStoryBob", "bobross.png"],
-    ["DansGame", "dansgame.png"],
-    ["NotLikeThis", "notlikethis.png"],
-    ["PogChamp", "pogchamp.png"],
-    ["catJAM", "catJAM.gif"],
-    ["KEKW", "KEKW.png"],
-    ["OMEGALUL", "OMEGALUL.png"],
-    ["PepeLaugh", "PepeLaugh.png"],
-    ["Clap", "Clap.gif"],
-    ["LULW", "LULW.png"],
-    ["SeriousSloth", "SeriousSloth.png"],
-    ["TriHard", "TriHard.png"],
-    ["Poggers", "Poggers.png"],
-    ["BibleThump", "BibleThump.png"],
-    ["Kreygasm", "Kreygasm.png"]
-];
-
-
 var spamSpeed = 1200;
-
-
 
 
 //is called automatically when the html page is loaded
@@ -94,10 +51,14 @@ function getMessage()
 
 function replace_emotes(message)
 {
-    for(var i=0;i<emotes.length;i++){
-        message = message.replace(new RegExp(emotes[i][0], 'g'), "<img src='pics/twitch_emotes/"+emotes[i][1]+"' alt='"+emotes[i][0]+"'>");
+    message = " " + message + " "; //add space before and after
+
+    for (var i=0;i<emotes.length;i++)
+    {
+        message = message.replace(new RegExp(" " + emotes[i][0] + " ", 'g'), " <img src='pics/twitch_emotes/"+emotes[i][1]+"' alt='"+emotes[i][0]+"'> ");
     }
 
+    message = message.slice(1, -1); //remove the added spaces
     return message;
 }
 
